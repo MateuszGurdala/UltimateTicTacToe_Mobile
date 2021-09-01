1a8c7aa6ae3801f63a3036d1baaea76cc717280d
Mateusz <mateusz.gurdala.mg@gmail.com>
2021-09-01 16:00:20 +0200

    Added methods for choosing segments

--- game_map.py
+++ game_map.py
@@ -1,3 +1,5 @@
+import copy
+
 from imports import *
 from segment import Segment
 
@@ -33,8 +35,29 @@ class GameMap(FloatLayout):
         for i in self.segments:
             for j in self.segments[i].places:
                 self.segments[i].places[j].disabled = True
+                Window.unbind(mouse_pos=self.segments[i].places[j].on_mouse_pos)
 
     def activate_segment(self, segment_number):
         for i in self.segments[segment_number].places:
             if not self.segments[segment_number].places[i].if_disabled:
                 self.segments[segment_number].places[i].disabled = False
+                Window.bind(mouse_pos=self.segments[segment_number].places[i].on_mouse_pos)
+
+    def create_choose_button(self, segment_number, if_disabled=False):
+        segment = self.segments[segment_number]
+        # It looks terrible but everything else failed
+        root = self.parent.parent.parent.parent.parent.parent
+        button = Button(pos=segment.pos,
+                        size=segment.size,
+                        background_normal="graphics/game_map/test.png",
+                        background_down="graphics/game_map/test.png",
+                        on_release=lambda a: root.set_current_segment(segment_number),
+                        disabled=if_disabled)
+        self.segments[segment_number].choose_button = button
+        self.segments[segment_number].add_widget(self.segments[segment_number].choose_button)
+
+    def remove_choose_button(self, segment_number):
+        segment = self.segments[segment_number]
+        if segment.choose_button:
+            segment.remove_widget(segment.choose_button)
+            segment.choose_button = None
