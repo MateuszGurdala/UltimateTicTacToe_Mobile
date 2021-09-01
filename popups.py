0a7a70bb39922521a69aec6f4da69d0c66c2bd21
Mateusz <mateusz.gurdala.mg@gmail.com>
2021-09-01 16:03:27 +0200

    Started creating winner/return_to_menu popups

---
+++ popups.py
@@ -0,0 +1,13 @@
+from imports import *
+
+Builder.load_file("popups.kv")
+
+
+class ReturnPopup(Popup):
+    def __init__(self, *args, **kwargs):
+        super(ReturnPopup, self).__init__(*args, **kwargs)
+
+
+class WinnerPopup(Popup):
+    def __init__(self, *args, **kwargs):
+        super(WinnerPopup, self).__init__(*args, **kwargs)
