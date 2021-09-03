af9cd5dfc8aed38463ce6f8591a1068b612a1703
Mateusz <mateusz.gurdala.mg@gmail.com>
2021-09-03 02:19:32 +0200

    Added "Return to menu" popup

--- popups.py
+++ popups.py
@@ -8,6 +8,7 @@ class ReturnPopup(Popup):
         super(ReturnPopup, self).__init__(*args, **kwargs)
 
 
+
 class WinnerPopup(Popup):
     def __init__(self, *args, **kwargs):
         super(WinnerPopup, self).__init__(*args, **kwargs)
