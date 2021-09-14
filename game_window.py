b811b9139ba6f7c121d05af630f4154f795dd9b3
Mateusz <mateusz.gurdala.mg@gmail.com>
2021-09-14 19:06:00 +0200

    Added current player display

--- game_window.py
+++ game_window.py
@@ -15,14 +15,14 @@ class GameWindow(Widget):
         self.game_map = None
         self.game_map_layout = None
         self.game_log_layout = None
-
-        self.log_extended = False
+        self.current_player_display = None
 
     def create_references(self):
         # Creating Game Map reference
         self.game_map = self.ids["game_map"].__self__
         self.game_log_layout = self.ids["game_log_layout"].__self__
         self.game_map_layout = self.ids["game_map_layout"].__self__
+        self.current_player_display = self.ids["current_player_display"].__self__
 
         # Creating references to all segments and places
         for i in self.game_map.ids:
@@ -30,22 +30,18 @@ class GameWindow(Widget):
             for j in self.game_map.segments[i].ids:
                 self.game_map.segments[i].places[j] = self.game_map.segments[i].ids[j].__self__
 
+    def set_display(self):
+        self.current_player_display.source = self.parent.parent.game_settings_screen.display_player.source
+
     def place_sign(self, segment_number, place_number, sign_graphic):
         self.game_map.segments[segment_number].places[place_number].source = sign_graphic
-
-    def extend_game_log(self):
-        if not self.log_extended:
-            animation.extend_log(self.game_log_layout, self.game_map_layout, 3 / 13)
-            self.log_extended = True
-        elif self.log_extended:
-            animation.hide_log(self.game_log_layout, self.game_map_layout, 12.5 / 13)
-            self.log_extended = False
 
     def reset_game_map(self):
         Clock.schedule_once(lambda a: self.game_map.reset(), 1)
 
     def discover_segments(self):
         Clock.schedule_once(lambda a: self.game_map.discover_segments(), 1)
+        Clock.schedule_once(lambda a: self.set_display(), 3)
 
     def hide_segments(self):
         Clock.schedule_once(lambda a: self.game_map.hide_segments(), 1)
@@ -65,7 +61,7 @@ class GameWindow(Widget):
             self.game_map.segments[i].highlight = None
             self.game_map.segments[i].canvas.before.clear()
 
-    def create_choose_segment_buttons(self, if_disabled=False):
+    def create_choose_segment_buttons(self):
         for i in self.game_map.segments:
             if not self.game_map.segments[i].highlight:
                 self.game_map.create_choose_button(i)
